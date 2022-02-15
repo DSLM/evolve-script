a5b98875496cab0a0154502b7f210fda1da91698
DSLM <714255036@qq.com>
2022-02-14 22:10:55 -0500

    Revert "10秒资源支撑"
    This reverts commit dce721e88d07385fe456c39bb7c9d36700d555c4.

--- selfUse.py
+++ selfUse.py
@@ -7,10 +7,10 @@ content = oldFile.read()
 
 replaceLsit = {
     "// @downloadURL  https://gitee.com/by22dgb/evolvescript/raw/master/evolve_automation.user.js" : "// @downloadURL  https://github.com/DSLM/evolve-script/raw/master/evolve_automation_DSLM.user.js",
-    "// ==/UserScript==" : "// ==/UserScript==\n//炼钢、冶炼和石墨烯厂会在需求资源储量可以支撑运作10秒以上时继续运作而非关闭\n//让部分背景色随主题变化",
+    "// ==/UserScript==" : "// ==/UserScript==\n//炼钢、冶炼和石墨烯厂会在需求资源储量大于10万时继续运作而非关闭\n//让部分背景色随主题变化",
 
     #炼钢，冶炼，石墨烯厂
-    "if (resource.storageRatio < 0.8" : "if (resource.storageRatio < 0.8 && ((resource.currentQuantity/resource.rateOfChange)>-10 && (resource.currentQuantity/resource.rateOfChange)<0)",
+    "if (resource.storageRatio < 0.8" : "if (resource.storageRatio < 0.8 && resource.currentQuantity < 100000",
     #背景色
     "background-color: #1f2424;" : "border: #ccc solid 0.0625rem;",
     '<div class="script-modal-content">' : '<div class="script-modal-content resource alt">',
